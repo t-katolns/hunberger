e9d36294f3fbe6804456173ff118ae23917d2e74
Ttt Iii <tk.katotai11@gmail.com>
2020-07-13 23:04:16 +0900

    テスト

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Nav/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
